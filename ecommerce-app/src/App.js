a557ae983c72802599e766e9d6eef154430ee5cd
Kanhaiya Yadav <yadukanhaiya111@gmail.com>
2022-03-16 21:21:21 +0530

    deleted some lines

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Kanhaiya</h1>
       <h1>Hello World</h1>
-      <h1>Hello Boss</h1>
+     
     </div>
   );
 }
