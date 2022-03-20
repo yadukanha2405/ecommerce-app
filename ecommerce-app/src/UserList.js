54c3555f77012b1aece1e4b2797c39276f172647
Kanhaiya Yadav <yadukanhaiya111@gmail.com>
2022-03-20 22:45:59 +0530

    Add user task

--- ecommerce-app/src/UserList.js
+++ ecommerce-app/src/UserList.js
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux"
+
+
 export default function UserList(){
+    const data = useSelector((state)=>state.user)
+        
     return (
-        <div>
+        <div className="main-item-container">
             <h1>UserList Componennt</h1>
+            {data.map((item, index) => (
+                <div  className="item-list" >
+                    <p> Name : {item.name} </p>
+                    <p> Email-Id : {item.email} </p>
+                </div>
+            ))
+
+            }
+           
         </div>
     )
 }
